69e243119c22597f6aa8e3e57e15a4a93cb851e1
Valera <valera.krivorot@gmail.com>
2020-09-17 19:52:04 +0300

    1

--- hollway.py
+++ hollway.py
@@ -15,8 +15,11 @@ def run_bot():
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))  # создается обработчик команды старт
     dispatcher.add_handler(CommandHandler("math", math))
-    dispatcher.add_handler(PrefixHandler("#", "random", get_num))
-    dispatcher.add_handler(PrefixHandler("#", "count", get_count))
+    dispatcher.add_handler(PrefixHandler("/", "command", command))
+    dispatcher.add_handler(PrefixHandler("/", "random", get_num))
+    dispatcher.add_handler(PrefixHandler("/", "count", get_count))
+    dispatcher.add_handler(PrefixHandler("!", "save", save_file))
+    dispatcher.add_handler(PrefixHandler("!", "read", read_file))
     dispatcher.add_handler(MessageHandler(Filters.text, message)) # создается обработчик простого сообщения
     updater.start_polling() #создает несколько подключений к апи и запускаетсяя проверка
     updater.idle()
@@ -33,11 +36,8 @@ def message(bot, context):
     text = text.lower()
     hyext = text[::-1]
 
-
-
-
     if text == "привет":
-        bot.message.reply_text("Хует")
+        bot.message.reply_text("Привет")
     elif text == "ky":
         bot.message.reply_text("ky")
     else: bot.message.reply_text(hyext)
@@ -50,8 +50,35 @@ def get_count(bot, context):
     bot.message.reply_text(str(count))
 
 
+def command (bot, context):
+    bot.message.reply_text("Используемые команды:\n"
+                            "/command\n"
+                            "/random\n"
+                            "/command\n"
+                            "!creation (file.txt) (info)\n"
+                            "!read (file.txt)\n"
+                            "!add (file.txt) (info)")
+
+
 def get_num(bot, context):
     bot.message.reply_text(str(random.random(10)))
+
+
+def save_file(bot, context):
+    #print(context.args)
+    file_name, text = context.args[0], " ".join(context.args[1:]) #через обычное открытие (обязательно закрыть файл!!)
+    file = open(file_name, "w")
+    res = file.write(text)
+    bot.message.reply_text(str(res))
+    file.close()
+
+
+
+def read_file(bot, context):
+    #print(context.args)
+    file_name = context.args[0]
+    with open(file_name):                            #через контекстный менеджер, не нужно закрывать файл
+        bot.message.reply_text(file.read())
 
 
 if __name__ == "__main__":
